d62563922fb21cb345d4884fb0958c6427f29714
noah18c <noahcroes@gmail.com>
2023-06-02 21:35:50 +0200

    some more tests added

--- src/main/java/assignment8/test/GameTest.java
+++ src/main/java/assignment8/test/GameTest.java
@@ -104,4 +104,47 @@ public class GameTest {
         boolean result = game.didPlayerWin(player);
         Assertions.assertFalse(result);
     }
+
+    @Test
+    public void testAddMultiplePlayers() {
+        Game game = new Game(50);
+        game.add("Player1");
+        game.add("Player2");
+        game.add("Player3");
+        Assertions.assertEquals(3, game.getPlayerHandler().getPlayers().size());
+    }
+
+    @Test
+    public void testRoll_WithPlayerInPenaltyBoxAndMaxRoll() {
+        Game game = new Game(50);
+        game.add("Player1");
+        Player player = game.getPlayerHandler().getCurrentPlayer();
+        player.setInPenaltyBox(true);
+        game.roll(12);
+        Assertions.assertTrue(player.isInPenaltyBox());
+        Assertions.assertEquals(0, player.getPlace());
+    }
+
+    @Test
+    public void testWasCorrectlyAnswered_WithPlayerInPenaltyBoxAndMaxCoins() {
+        Game game = new Game(50);
+        game.add("Player1");
+        Player player = game.getPlayerHandler().getCurrentPlayer();
+        player.setInPenaltyBox(true);
+        for (int i = 0; i < 6; i++)
+            player.giveCoins();
+        boolean result = game.wasCorrectlyAnswered();
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void testWrongAnswer_WithPlayerInPenaltyBox() {
+        Game game = new Game(50);
+        game.add("Player1");
+        Player player = game.getPlayerHandler().getCurrentPlayer();
+        player.setInPenaltyBox(true);
+        game.wrongAnswer();
+        Assertions.assertTrue(player.isInPenaltyBox());
+        Assertions.assertEquals(0, player.getPlace());
+    }
 }
